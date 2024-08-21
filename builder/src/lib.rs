416f991b54e760618c70059bb1b0b15657419e6d
Vera Gonzalez <me@vera.lgbt>
2024-08-21 14:17:30 -0400

    p1t6 - starting

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,12 +1,41 @@
-use proc_macro::TokenStream;
+use proc_macro::{Span, TokenStream};
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Data, DeriveInput, Fields, GenericArgument, Ident, Type};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
 
+    let option_ident = Ident::new("Option", Span::call_site().into());
+
     println!("ast: {:#?}", ast);
+    if let Data::Struct(data_struct) = ast.data.clone() {
+        if let Fields::Named(namedfields) = data_struct.fields {
+            //namedfields.named.into_iter().map(|v} v.)
+            for field in namedfields.named {
+                let field_ident = field.ident;
+                let field_type = field.ty.clone();
+                if let Type::Path(type_path) = field.ty {
+                    if type_path.path.segments[0].ident == option_ident {
+                        if let syn::PathArguments::AngleBracketed(args) =
+                            &type_path.path.segments[0].arguments
+                        {
+                            if let GenericArgument::Type(field_type) = &args.args[0] {
+
+                                // (field_ident, t, true)
+                            }
+                        }
+                    }
+                }
+            }
+
+            println!("WARNING: this AST isn't applicable!");
+            return quote! {
+                #ast
+            }
+            .into();
+        }
+    }
 
     let ident = ast.ident.clone();
     let builder_ident = format_ident!("{}{}", ident, "Builder");
@@ -52,6 +81,9 @@ pub fn derive(input: TokenStream) -> TokenStream {
             }
 
             fn build(&mut self) -> Result<#ident, Box<dyn std::error::Error>> {
+
+                if ast.
+
                 match (&self.executable, &self.args, &self.env, &self.current_dir) {
                     (Some(executable), Some(args), Some(env), Some(current_dir)) => Ok(#ident {
                         executable: executable.clone(),
